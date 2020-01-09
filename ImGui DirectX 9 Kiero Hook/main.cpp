abf5685e2cfbfd576ac0185e34c8dff5c7554ee1
Rdbo <57117082+rdbo@users.noreply.github.com>
2020-01-09 13:34:01 -0300

    Add files via upload

---
+++ ImGui DirectX 9 Kiero Hook/main.cpp
@@ -0,0 +1,77 @@
+#include "includes.h"
+
+extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+EndScene oEndScene = NULL;
+WNDPROC oWndProc;
+static HWND window;
+
+void InitImGui(LPDIRECT3DDEVICE9 pDevice)
+{
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags = ImGuiConfigFlags_NoMouseCursorChange;
+	ImGui_ImplWin32_Init(window);
+	ImGui_ImplDX9_Init(pDevice);
+}
+
+bool init = false;
+long __stdcall hkEndScene(LPDIRECT3DDEVICE9 pDevice)
+{
+	if (!init)
+	{
+		InitImGui(pDevice);
+		init = true;
+	}
+
+	ImGui_ImplDX9_NewFrame();
+	ImGui_ImplWin32_NewFrame();
+	ImGui::NewFrame();
+
+	ImGui::Begin("ImGui Window");
+	ImGui::End();
+
+	ImGui::EndFrame();
+	ImGui::Render();
+	ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+
+	return oEndScene(pDevice);
+}
+
+LRESULT __stdcall WndProc(const HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+
+	if (true && ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam))
+		return true;
+
+	return CallWindowProc(oWndProc, hWnd, uMsg, wParam, lParam);
+}
+
+DWORD WINAPI MainThread(LPVOID lpReserved)
+{
+	bool attached = false;
+	do
+	{
+		if (kiero::init(kiero::RenderType::D3D9) == kiero::Status::Success)
+		{
+			kiero::bind(42, (void**)& oEndScene, hkEndScene);
+			window = FindWindow(NULL, WINDOW_NAME);
+			oWndProc = (WNDPROC)SetWindowLongPtr(window, GWL_WNDPROC, (LONG_PTR)WndProc);
+		}
+	} while (!attached);
+	return TRUE;
+}
+
+BOOL WINAPI DllMain(HMODULE hMod, DWORD dwReason, LPVOID lpReserved)
+{
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(hMod);
+		CreateThread(nullptr, 0, MainThread, hMod, 0, nullptr);
+		break;
+	case DLL_PROCESS_DETACH:
+		kiero::shutdown();
+		break;
+	}
+	return TRUE;
+}
